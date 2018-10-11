5aff9dc1b167cc5b465978ecac5c95ecb377c0d8
Philippe WRONSKI <philippe.wronski@gmail.com>
2018-10-11 14:09:37 +0200

    Création des composants appbar et  toolbar

---
+++ src/components/commons/app-bar/AppBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppBar = ({children}) => (
+  <div className="app-bar">
+    {children}
+  </div>
+)
+
+export default AppBar
---
+++ src/components/commons/toolbar/Toolbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Toolbar = ({children}) => (
+  <div className="tool-bar">
+    {children}
+  </div>
+)
+
+export default Toolbar
